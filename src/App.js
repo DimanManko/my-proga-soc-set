3114195fc44cec72ff378b874cf88047e3e178c4
Dmitriy Manko <shevelevman@gmail.com>
2024-09-22 21:40:11 +1000

    проверка

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import {findAllByDisplayValue} from "@testing-library/react";
+
+
+function App() {
+    return (
+
+        <div>
+            <div>
+                <h1> "My friends"</h1>
+            </div>
+        </div>
+
+
+    );
+
+}
+
+
+
+/*
+<PageTitle title = {"My friends"}/>
+function PageTitle() {
+    return <h1>{title}</h1>;
+*/
+
+export default App;
+
+
