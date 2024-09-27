f607fed073fe7b06f2c1a8cf74918509fbaabafa
Dmitriy Manko <shevelevman@gmail.com>
2024-09-27 20:22:29 +1000

    Настройка компонент (props)

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import React from 'react';
-import './App.css
+import './App.css'
+import Accordion from "./components/Accordion.tsx";
+import {Rating} from "./components/Rating";
 
 function App() {
     return (
-<h1></h1>
         <div>
-            <div>
-                <h1> "My friends"</h1>
-            </div>
+            <PageTitle title={"My friends"}/>
+            <Accordion titleValue={"Menu"}/>
+            <Accordion titleValue={"Users"}/>
+            <Rating value={6}/>
         </div>
-
-
     );
-
 }
 
+function PageTitle(props: any) {
+    return <h1>{props.title}</h1>
+}
 
-
-/*
-<PageTitle title = {"My friends"}/>
-function PageTitle() {
-    return <h1>{title}</h1>;
-*/
 
 export default App;
 
