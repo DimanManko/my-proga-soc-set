5d656912face76d5b369aa9421d9cc087cafdba5
DimanManko <shevelevman@gmail.com>
2025-04-19 15:05:57 +1000

    41 - React JS Практика - reducer (почти redux)

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from "./Dialogs.module.css";
 import {DialogItem} from "./DialogItem/DialogItem";
 import {Message} from "./Message/Message";
-import {sendMessageCreator, updateNewMessageBodyCreator} from "../../redux/state";
+import {sendMessageCreator, updateNewMessageBodyCreator} from "../../redux/dialogs-reducer";
 
 
 
@@ -13,6 +13,7 @@ const Dialogs = (props) => {
     let dialogsElements = state.dialogs.map (d => <DialogItem name={d.name} id={d.id}/> )
     let messagesElements = state.messages.map (m => <Message message={m.message}/>)
     let newMessageBody = state.newMessageBody
+
     let onSendMessageClick = () => {
         props.store.dispatch(sendMessageCreator())
     }
---
+++ src/redux/sidebar-reducer.jsx
@@ -0,0 +1,7 @@
+import dialogsReducer from "./dialogs-reducer";
+
+
+const sidebarReducer = (state, action) => {
+    return state
+}
+export default sidebarReducer
