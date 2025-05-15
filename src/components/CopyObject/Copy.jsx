029e053656f304d7c183a16aeb8ff5314cec3147
DimanManko <shevelevman@gmail.com>
2025-05-15 11:09:55 +1000

    50 - 52 Подключение к серваку (API), Side Effect, далле будут Хуки

--- src/components/CopyObject/Copy.jsx
+++ src/components/CopyObject/Copy.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from "axios";
 
 
 function greet(name) {
@@ -10,4 +11,63 @@ function runCallback(cb) {
 }
 
 greet(runCallback); // Выводит на печать: «Привет, Алиса!» [3](https://sky.pro/wiki/javascript/peredacha-funktsiy-kak-parametrov-v-java-script-bez-eval/)
+
+let Users = [
+        {
+            id: 1,
+            photoUrl: "Dima.png",
+            followed: false,
+            fullName: "Dima",
+            status: "Я крутой программист",
+            location: {city: "Khabarovsk", country: "Russia"}
+        },
+        {
+            id: 2,
+            photoUrl: "Oleg.png",
+            followed: true,
+            fullName: "Oleg",
+            status: "Люблю кушать сосисечки",
+            location: {city: "Penza", country: "Russia"}
+        },
+        {
+            id: 3,
+            photoUrl: "AntonReva.png",
+            followed: false,
+            fullName: "Anton Reva",
+            status: "Крутой брокер, хочешь правильно вложиться - это ко мне!",
+            location: {city: "Piter", country: "Russia"}
+        },
+        {
+            id: 4,
+            photoUrl: "Nikita.png",
+            followed: true,
+            fullName: "Nikita",
+            status: "Рулька с утра, это всё о чём можно мечтать",
+            location: {city: "Vladivostok", country: "Russia"}
+        },
+        {
+            id: 5,
+            photoUrl: "Kot.png",
+            followed: false,
+            fullName: "Kot",
+            status: "Могу свалить с работы, сказав, что меня затапило",
+            location: {city: "Sochi", country: "Russia"}
+        },
+        {
+            id: 6,
+            photoUrl: "logo.png",
+            followed: true,
+            fullName: "Anton",
+            status: "Как же меня все достали",
+            location: {city: "Penza", country: "Russia"}
+        },
+        {
+            id: 7,
+            photoUrl: "logo.png",
+            followed: true,
+            fullName: "Artur",
+            status: "Играю в PUBG",
+            location: {city: "Khabarovsk", country: "Russia"}
+        }
+    ]
 
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,76 +1,25 @@
 import React from 'react';
 import styles from "./Users.module.css"
+import axios from "axios";
 
 let Users = (props) => {
 
-    if (props.users.length === 0) {
-
-        props.setUsers([
-            {
-                id: 1,
-                photoUrl: "Dima.png",
-                followed: false,
-                fullName: "Dima",
-                status: "Я крутой программист",
-                location: {city: "Khabarovsk", country: "Russia"}
-            },
-            {
-                id: 2,
-                photoUrl: "Oleg.png",
-                followed: true,
-                fullName: "Oleg",
-                status: "Люблю кушать сосисечки",
-                location: {city: "Penza", country: "Russia"}
-            },
-            {
-                id: 3,
-                photoUrl: "AntonReva.png",
-                followed: false,
-                fullName: "Anton Reva",
-                status: "Крутой брокер, хочешь правильно вложиться - это ко мне!",
-                location: {city: "Piter", country: "Russia"}
-            },
-            {
-                id: 4,
-                photoUrl: "Nikita.png",
-                followed: true,
-                fullName: "Nikita",
-                status: "Рулька с утра, это всё о чём можно мечтать",
-                location: {city: "Vladivostok", country: "Russia"}
-            },
-            {
-                id: 5,
-                photoUrl: "Kot.png",
-                followed: false,
-                fullName: "Kot",
-                status: "Могу свалить с работы, сказав, что меня затапило",
-                location: {city: "Sochi", country: "Russia"}
-            },
-            {
-                id: 6,
-                photoUrl: "logo.png",
-                followed: true,
-                fullName: "Anton",
-                status: "Как же меня все достали",
-                location: {city: "Penza", country: "Russia"}
-            },
-            {
-                id: 7,
-                photoUrl: "logo.png",
-                followed: true,
-                fullName: "Artur",
-                status: "Играю в PUBG",
-                location: {city: "Khabarovsk", country: "Russia"}
-            }
-        ])
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                props.setUsers(response.data.items)
+            })
+        }
     }
-
     return <div>
+
+        <button onClick ={getUsers}>Get Users</button>
+
         {
             props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src= {u.photoUrl} className={styles.userPhoto}/>
+                        <img src= {u.photos.small != null ? u.photos.small : "AntonReva.png"} className={styles.userPhoto}/>
                     </div>
                     <div>
                         {u.followed
@@ -80,15 +29,14 @@ let Users = (props) => {
                 </span>
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.Name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{"u.location.country"}</div>
+                        <div>{"u.location.city"}</div>
                     </span>
                 </span>
-
             </div>)
         }
     </div>
